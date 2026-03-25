560b41a638607af20015d0414152da5ef348896b
Bas van Eijk <bas.vaneijk@icloud.com>
2026-03-25 23:08:11 +0100

    perf: no cache

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/legal.css'],
+  app: {
+    head: {
+      meta: [
+        { 'http-equiv': 'Cache-Control', content: 'no-cache, no-store, must-revalidate' },
+        { 'http-equiv': 'Pragma', content: 'no-cache' },
+        { 'http-equiv': 'Expires', content: '0' },
+      ],
+    },
+  },
   nitro: {
     preset: 'github_pages',
   },
