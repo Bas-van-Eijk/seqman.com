207c5582e21fd06a552748b4610374a239878ca8
Bas van Eijk <bas.vaneijk@icloud.com>
2026-03-25 23:44:22 +0100

    feat: favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,11 @@ export default defineNuxtConfig({
         { 'http-equiv': 'Pragma', content: 'no-cache' },
         { 'http-equiv': 'Expires', content: '0' },
       ],
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      ],
     },
   },
   nitro: {
