46261cbfc754ec20feb52af9410f65119f5a0e4e
Bas van Eijk <bas.vaneijk@icloud.com>
2026-04-14 17:07:48 +0200

    changed vid

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -159,7 +159,7 @@ const faqItems = [
     <section :ref="setRevealRef" class="video-section">
       <div class="video-wrap">
         <iframe
-          src="https://www.youtube.com/embed/7Goe0BEkExI"
+          src="https://www.youtube.com/embed/TOGjxp666qI"
           title="StepDeq demo"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
